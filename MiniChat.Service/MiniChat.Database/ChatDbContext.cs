0e349b8a6fae8c5a7493cb31467d20722e5458c2
FremyXS <70861067+FremyXS@users.noreply.github.com>
2023-06-24 01:24:56 +0500

    feat(MiniChat.Database): add context ChatDbContext

---
+++ MiniChat.Service/MiniChat.Database/ChatDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiniChat.Database.Entity;
+
+namespace MiniChat.Database
+{
+    public class ChatDbContext : DbContext
+    {
+        public DbSet<ChatRoom> ChatRooms { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<User> Users { get; set; }
+        public ChatDbContext(DbContextOptions<ChatDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}

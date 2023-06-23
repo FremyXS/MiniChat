306063bddc47e9802a3e26e715dc1c27328a31cc
FremyXS <70861067+FremyXS@users.noreply.github.com>
2023-06-24 01:02:24 +0500

    feat(MiniChat.Database): add model User

---
+++ MiniChat.Service/MiniChat.Database/Entity/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniChat.Database.Entity
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Photo { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? DeleteDate { get; set; } = null;
+        public ICollection<ChatRoom> ChatRooms { get; set; }
+        public ICollection<Message> Messages { get; set; }
+    }
+}

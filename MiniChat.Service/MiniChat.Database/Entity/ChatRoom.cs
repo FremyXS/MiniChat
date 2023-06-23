816bcb7d0cc87d82ef744221768a3808897906b5
FremyXS <70861067+FremyXS@users.noreply.github.com>
2023-06-24 01:01:51 +0500

    feat(MiniChat.Database): add model ChatRoom

---
+++ MiniChat.Service/MiniChat.Database/Entity/ChatRoom.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniChat.Database.Entity
+{
+    public class ChatRoom
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Photo { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? DeleteDate { get; set; } = null;
+        public ICollection<Message> Messages { get; set; }
+    }
+}

817e273e0c3ce6ae88a82cfcada06d9096141804
FremyXS <70861067+FremyXS@users.noreply.github.com>
2023-06-24 14:36:56 +0500

    feat(MiniChat.Service): add DependencyInjection

---
+++ MiniChat.Service/MiniChat.Services/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MiniChat.Service.Commands;
+using MiniChat.Service.Commands.Interface;
+using MiniChat.Services.Service;
+using MiniChat.Services.Service.Interface;
+
+namespace MiniChat.Services
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddTransient<IGetUsersCommand, GetUsersCommand>();
+            services.AddTransient<ICreateUserCommand, CreateUserCommand>();
+            services.AddTransient<IUserService, UserService>();
+            return services;
+        }
+    }
+}

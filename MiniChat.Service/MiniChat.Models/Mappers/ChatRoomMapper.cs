403735c68665991a1181204cdb364a3577cdf15d
FremyXS <70861067+FremyXS@users.noreply.github.com>
2023-06-25 02:36:33 +0500

    feat(MiniChat.Models): add mapper ChatRoomMapper

---
+++ MiniChat.Service/MiniChat.Models/Mappers/ChatRoomMapper.cs
@@ -0,0 +1,25 @@
+﻿using Mapster;
+using MiniChat.Database.Entity;
+using MiniChat.Models.Request;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniChat.Models.Mappers
+{
+    public static class ChatRoomMapper
+    {
+        static ChatRoomMapper()
+        {
+            TypeAdapterConfig<ChatRoomCreateRequest, ChatRoom>
+                .NewConfig()
+                .Map(x => x.Users, src => new List<User>());
+        }
+        public static ChatRoom ToModel(this ChatRoomCreateRequest chatRoomCreateRequest)
+        {
+            return chatRoomCreateRequest.Adapt<ChatRoom>();
+        }
+    }
+}

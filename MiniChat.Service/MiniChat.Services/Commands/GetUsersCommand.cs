4b29c18d870efac70d6280f1643a8d66dcd2abe7
FremyXS <70861067+FremyXS@users.noreply.github.com>
2023-06-24 14:34:06 +0500

    feat(MiniChat.Service): add command GetUsersCommand

---
+++ MiniChat.Service/MiniChat.Services/Commands/GetUsersCommand.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiniChat.Database;
+using MiniChat.Database.Entity;
+using MiniChat.Service.Commands.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniChat.Service.Commands
+{
+    public class GetUsersCommand: IGetUsersCommand
+    {
+        private readonly ChatDbContext _chatDbContext;
+
+        public GetUsersCommand(ChatDbContext chatDbContext)
+        {
+            _chatDbContext = chatDbContext;
+        }
+
+        public async Task<ICollection<User>> Invoke()
+        {
+            var users = await _chatDbContext.Users.ToListAsync();
+            return users;
+        }
+    }
+}

24b335d0be1b518898f33037c430eb86c1f847e2
FremyXS <70861067+FremyXS@users.noreply.github.com>
2023-06-24 22:10:05 +0500

    feat(MiniChat.Models): add mapper UserMapper

---
+++ MiniChat.Service/MiniChat.Models/Mappers/UserMapper.cs
@@ -0,0 +1,14 @@
+﻿using Mapster;
+using MiniChat.Database.Entity;
+using MiniChat.Models.Dto;
+
+namespace MiniChat.Models.Mappers
+{
+    public static class UserMapper
+    {
+        public static UserDto ToDto(this User user)
+        {
+            return user.Adapt<UserDto>();
+        }
+    }
+}

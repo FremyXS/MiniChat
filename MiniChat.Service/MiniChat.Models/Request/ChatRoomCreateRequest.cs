ae7f3a3672c23ffe8fb793e291a1ad26bba32980
FremyXS <70861067+FremyXS@users.noreply.github.com>
2023-06-25 02:36:00 +0500

    feat(MiniChat.Models): add request ChatRoomCreateRequest

---
+++ MiniChat.Service/MiniChat.Models/Request/ChatRoomCreateRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniChat.Models.Request
+{
+    public class ChatRoomCreateRequest
+    {
+        public string Title { get; set; }
+        public string? Photo { get; set; }
+        public long[] UsersId { get; set; }
+    }
+}

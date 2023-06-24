c8edc668208dad3e09c8a82154735fc87f2213f8
FremyXS <70861067+FremyXS@users.noreply.github.com>
2023-06-24 14:35:47 +0500

    feat(MiniChat.Service): add service UserService

---
+++ MiniChat.Service/MiniChat.Services/Service/Interface/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using MiniChat.Database.Entity;
+using MiniChat.Models.Request;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniChat.Services.Service.Interface
+{
+    public interface IUserService
+    {
+        Task<ICollection<User>> GetUsers();
+        Task<int> CreateUser(UserRequest userRequest);
+    }
+}
---
+++ MiniChat.Service/MiniChat.Services/Service/UserService.cs
@@ -0,0 +1,35 @@
+﻿using MiniChat.Database.Entity;
+using MiniChat.Models.Request;
+using MiniChat.Service.Commands.Interface;
+using MiniChat.Services.Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniChat.Services.Service
+{
+    public class UserService : IUserService
+    {
+        private readonly IGetUsersCommand _getUsersCommand;
+        private readonly ICreateUserCommand _createUserCommand;
+        public UserService(
+            IGetUsersCommand getUsersCommand,
+            ICreateUserCommand createUserCommand) 
+        { 
+            _getUsersCommand = getUsersCommand;
+            _createUserCommand = createUserCommand;
+        }
+
+        public async Task<ICollection<User>> GetUsers()
+        {
+            return await _getUsersCommand.Invoke();
+        }
+
+        public async Task<int> CreateUser(UserRequest userRequest)
+        {
+            return await _createUserCommand.Invoke(userRequest);
+        }
+    }
+}

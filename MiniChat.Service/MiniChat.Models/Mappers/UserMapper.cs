49b3c648d1af056ac9c39183d7784c1d5e1725d8
FremyXS <70861067+FremyXS@users.noreply.github.com>
2023-06-24 22:28:20 +0500

    feat(MiniChat.Models): add mapper from UserCreateRequest to User model

--- MiniChat.Service/MiniChat.Models/Mappers/UserMapper.cs
+++ MiniChat.Service/MiniChat.Models/Mappers/UserMapper.cs
@@ -1,6 +1,7 @@
 ﻿using Mapster;
 using MiniChat.Database.Entity;
 using MiniChat.Models.Dto;
+using MiniChat.Models.Request;
 
 namespace MiniChat.Models.Mappers
 {
@@ -9,6 +10,11 @@ namespace MiniChat.Models.Mappers
         public static UserDto ToDto(this User user)
         {
             return user.Adapt<UserDto>();
+        }
+
+        public static User ToModel(this UserCreateRequest user)
+        {
+            return user.Adapt<User>();
         }
     }
 }

0450133e1e5cc00b9a74d0458d038fdcb8621181
FremyXS <70861067+FremyXS@users.noreply.github.com>
2023-06-25 19:17:54 +0500

    refactor(MiniChat.Models): change mapper MessageMapper - add new adapt from Message to MessageDto

--- MiniChat.Service/MiniChat.Models/Mappers/MessageMapper.cs
+++ MiniChat.Service/MiniChat.Models/Mappers/MessageMapper.cs
@@ -1,20 +1,26 @@
 ﻿using Mapster;
 using MiniChat.Database.Entity;
+using MiniChat.Models.Dto;
 using MiniChat.Models.Request;
 
 namespace MiniChat.Models.Mappers
 {
     public static class MessageMapper
     {
-        //static MessageMapper()
-        //{
-        //    TypeAdapterConfig<MessageCreateRequest, Message>
-        //        .NewConfig()
-        //        .Map(x => x.ChatRoom)
-        //}
+        static MessageMapper()
+        {
+            TypeAdapterConfig<Message, MessageDto>
+                .NewConfig()
+                .Map(x => x.UserName, src => src.User.Name);
+        }
         public static Message ToModel(this MessageCreateRequest message)
         {
             return message.Adapt<Message>();
+        }
+
+        public static MessageDto ToDto(this Message message)
+        {
+            return message.Adapt<MessageDto>();
         }
     }
 }

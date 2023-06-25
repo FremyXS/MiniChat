688ef41f80ebe1764887e007832ce52cb313156f
FremyXS <70861067+FremyXS@users.noreply.github.com>
2023-06-25 13:01:17 +0500

    feat(MiniChat.Models): add dto ChatRoomDto

---
+++ MiniChat.Service/MiniChat.Models/Dto/ChatRoomDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniChat.Models.Dto
+{
+    public class ChatRoomDto
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string? Photo { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}

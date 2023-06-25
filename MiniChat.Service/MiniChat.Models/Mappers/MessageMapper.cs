2de9f00b480e9ba713ba4edf0f3851e5be99d1f0
FremyXS <70861067+FremyXS@users.noreply.github.com>
2023-06-25 18:04:43 +0500

    feat(MiniChat.Models): add new mapper MessageMapper

---
+++ MiniChat.Service/MiniChat.Models/Mappers/MessageMapper.cs
@@ -0,0 +1,20 @@
+﻿using Mapster;
+using MiniChat.Database.Entity;
+using MiniChat.Models.Request;
+
+namespace MiniChat.Models.Mappers
+{
+    public static class MessageMapper
+    {
+        //static MessageMapper()
+        //{
+        //    TypeAdapterConfig<MessageCreateRequest, Message>
+        //        .NewConfig()
+        //        .Map(x => x.ChatRoom)
+        //}
+        public static Message ToModel(this MessageCreateRequest message)
+        {
+            return message.Adapt<Message>();
+        }
+    }
+}

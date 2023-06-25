1e6c1aa60d5f96ee36101853b823533123ca9018
FremyXS <70861067+FremyXS@users.noreply.github.com>
2023-06-25 19:38:59 +0500

    feat(MiniChat.Services): add new command GetMessageById

---
+++ MiniChat.Service/MiniChat.Services/Commands/GetMessageById.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiniChat.Database;
+using MiniChat.Database.Entity;
+using MiniChat.Service.Commands.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniChat.Service.Commands
+{
+    public class GetMessageById: IGetMessageById
+    {
+        private readonly ChatDbContext _chatDbContext;
+
+        public GetMessageById(ChatDbContext chatDbContext)
+        {
+            _chatDbContext = chatDbContext;
+        }
+
+        public async Task<Message> Invoke(long id)
+        {
+            var message = await _chatDbContext.Messages
+                .Include(el => el.User)
+                .Include(el => el.ChatRoom)
+                .FirstOrDefaultAsync(el => el.Id == id);
+
+            if(message == null)
+            {
+                throw new Exception($"Message is not found by id: {id}");
+            }
+
+            return message;
+        }
+    }
+}

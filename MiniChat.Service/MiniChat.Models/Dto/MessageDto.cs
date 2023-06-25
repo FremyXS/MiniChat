64591930799784e3c24294c6fa7c5119abdf1831
FremyXS <70861067+FremyXS@users.noreply.github.com>
2023-06-25 19:16:41 +0500

    feat(MiniChat.Models): add new dto MessageDto

---
+++ MiniChat.Service/MiniChat.Models/Dto/MessageDto.cs
@@ -0,0 +1,19 @@
+﻿using MiniChat.Database.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniChat.Models.Dto
+{
+    public class MessageDto
+    {
+        public long Id { get; set; }
+        public string Text { get; set; }
+        public bool IsRedaction { get; set; }
+        public DateTime CreateDate { get; set; }
+        public long UserId { get; set; }
+        public string UserName { get; set; }
+    }
+}

9284d8ba50b25b1fb7cb419e7571232e370c49ca
FremyXS <70861067+FremyXS@users.noreply.github.com>
2023-06-25 13:04:15 +0500

    feat(MiniChat.Services): add command GetChatRoomsByUserIdCommand

---
+++ MiniChat.Service/MiniChat.Services/Commands/GetChatRoomsByUserIdCommand.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiniChat.Database;
+using MiniChat.Models.Dto;
+using MiniChat.Models.Mappers;
+using MiniChat.Service.Commands.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniChat.Service.Commands
+{
+    public class GetChatRoomsByUserIdCommand: IGetChatRoomsByUserIdCommand
+    {
+        private readonly ChatDbContext _chatDbContext;
+        public GetChatRoomsByUserIdCommand(ChatDbContext chatDbContext)
+        {
+            _chatDbContext = chatDbContext;
+        }
+
+        public async Task<ICollection<ChatRoomDto>> Invoke(long userId)
+        {
+            var user = await _chatDbContext.Users
+                .Include(el => el.ChatRooms)
+                .FirstOrDefaultAsync(el => el.Id == userId);
+
+            if (user == null)
+            {
+                throw new Exception($"User is not found by id: {userId}");
+            }
+
+            var res = user.ChatRooms.Select(el => el.ToDto()).ToList();
+
+            return res;
+        }
+    }
+}

bcf6e9c68364b43d3eb8587c2d87f0fac79526a5
FremyXS <70861067+FremyXS@users.noreply.github.com>
2023-06-26 00:58:42 +0500

    feat(MiniChat.Web): add controller AuthController

--- MiniChat.Service/MiniChat.Web/Controllers/AuthController.cs
+++ MiniChat.Service/MiniChat.Web/Controllers/AuthController.cs
@@ -5,6 +5,22 @@ namespace MiniChat.Web.Controllers
     [Route("[controller]")]
     public class AuthController: ControllerBase
     {
+        [HttpPost("register")]
+        public async Task<IActionResult> Register()
+        {
+            return Ok();
+        }
 
+        [HttpPost("login")]
+        public async Task<IActionResult> Login()
+        {
+            return Ok();
+        }
+
+        [HttpPost("logout")]
+        public async Task<IActionResult> Logout()
+        {
+            return Ok();
+        }
     }
 }

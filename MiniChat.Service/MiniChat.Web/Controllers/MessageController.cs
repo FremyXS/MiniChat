1ce559b1767876f458214d789f3e267b34e40755
FremyXS <70861067+FremyXS@users.noreply.github.com>
2023-06-25 19:52:58 +0500

    refactor(MiniChat.Web): change MessageController - change endpoints cod

--- MiniChat.Service/MiniChat.Web/Controllers/MessageController.cs
+++ MiniChat.Service/MiniChat.Web/Controllers/MessageController.cs
@@ -1,32 +1,73 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MiniChat.Models.Request;
+using MiniChat.Service.Service.Interface;
 
 namespace MiniChat.Web.Controllers
 {
     [Route("[controller]")]
     public class MessageController : ControllerBase
     {
+        private readonly IMessageService _messageService;
+
+        public MessageController(IMessageService messageService)
+        {
+            _messageService = messageService;
+        }
+
         [HttpGet("chat/{chatId:long}")]
         public async Task<IActionResult> GetMessagesByChatId([FromRoute] long chatId)
         {
-            return Ok();
+            try
+            {
+                var res = await _messageService.GetMessagesByChatId(chatId);
+                return Ok(res);
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
         }
 
         [HttpPost]
-        public async Task<IActionResult> CreateMessage()
+        public async Task<IActionResult> CreateMessage([FromBody] MessageCreateRequest messageCreateRequest)
         {
-            return Ok();
+            try
+            {
+                var res = await _messageService.CreateMessage(messageCreateRequest);
+                return Ok(res);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpPatch("{id:long}")]
-        public async Task<IActionResult> UpdateMessage([FromRoute] long id)
+        public async Task<IActionResult> UpdateMessage([FromRoute] long id, [FromBody] MessageUpdateRequest messageUpdateRequest)
         {
-            return Ok();
+            try
+            {
+                var res = await _messageService.UpdateMessage(id, messageUpdateRequest);
+                return Ok(res);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpDelete("{id:long}")]
         public async Task<IActionResult> DeleteMessage([FromRoute] long id)
         {
-            return Ok();
+            try
+            {
+                var res = await _messageService.DeleteMessage(id);
+                return Ok(res);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }

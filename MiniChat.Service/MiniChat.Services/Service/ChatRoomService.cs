d3c40650710fd8a381a0cbd83d8b8411cc26e50b
FremyXS <70861067+FremyXS@users.noreply.github.com>
2023-06-25 16:28:28 +0500

    refactor(MiniChat.Service): change ChatRoomService: add validators

--- MiniChat.Service/MiniChat.Services/Service/ChatRoomService.cs
+++ MiniChat.Service/MiniChat.Services/Service/ChatRoomService.cs
@@ -1,6 +1,8 @@
-﻿using MiniChat.Models.Dto;
+﻿using FluentValidation;
+using MiniChat.Models.Dto;
 using MiniChat.Models.Mappers;
 using MiniChat.Models.Request;
+using MiniChat.Models.Validators;
 using MiniChat.Service.Commands.Interface;
 using MiniChat.Service.Service.Interface;
 using System;
@@ -17,19 +19,25 @@ namespace MiniChat.Service.Service
         private readonly IGetChatRoomByIdCommand _getChatRoomByIdCommand;
         private readonly ICreateChatRoomCommand _createChatRoomCommand;
         private readonly IUpdateChatRoomCommand _updateChatRoomCommand;
-        private readonly IDeleteChatRoom _deleteChatRoom;
+        private readonly IDeleteChatRoomCommand _deleteChatRoom;
+        private readonly ChatRoomCreateRequestValidator _chatRoomCreateRequestValidator;
+        private readonly ChatRoomUpdateRequestValidator _chatRoomUpdateRequestValidator;
         public ChatRoomService(
             IGetChatRoomsByUserIdCommand getChatRoomsByUserIdCommand,
             IGetChatRoomByIdCommand getChatRoomByIdCommand,
             ICreateChatRoomCommand createChatRoomCommand,
             IUpdateChatRoomCommand updateChatRoomCommand,
-            IDeleteChatRoom deleteChatRoom) 
+            IDeleteChatRoomCommand deleteChatRoom,
+            ChatRoomCreateRequestValidator chatRoomCreateRequestValidator,
+            ChatRoomUpdateRequestValidator chatRoomUpdateRequestValidator) 
         {
             _getChatRoomsByUserIdCommand = getChatRoomsByUserIdCommand;
             _getChatRoomByIdCommand = getChatRoomByIdCommand;
             _createChatRoomCommand = createChatRoomCommand;
             _updateChatRoomCommand = updateChatRoomCommand;
             _deleteChatRoom = deleteChatRoom;
+            _chatRoomCreateRequestValidator = chatRoomCreateRequestValidator;
+            _chatRoomUpdateRequestValidator = chatRoomUpdateRequestValidator;
         }
 
         public async Task<ICollection<ChatRoomDto>> GetChatRoomsByUserId(long userId)
@@ -42,10 +50,24 @@ namespace MiniChat.Service.Service
         }
         public async Task<int> CreateChatRoom(ChatRoomCreateRequest chatRoomCreateRequest)
         {
+            var validResult = await _chatRoomCreateRequestValidator.ValidateAsync(chatRoomCreateRequest);
+
+            if (!validResult.IsValid)
+            {
+                throw new ValidationException(validResult.Errors);
+            }
+
             return await _createChatRoomCommand.Invoke(chatRoomCreateRequest);
         }
         public async Task<int> UpdateChatRoom(long id, ChatRoomUpdateRequest chatRoomUpdateRequest)
         {
+            var validResult = await _chatRoomUpdateRequestValidator.ValidateAsync(chatRoomUpdateRequest);
+
+            if (!validResult.IsValid)
+            {
+                throw new ValidationException(validResult.Errors);
+            }
+
             return await _updateChatRoomCommand.Invoke(id, chatRoomUpdateRequest);
         }
         public async Task<int> DeleteChatRoom(long id)

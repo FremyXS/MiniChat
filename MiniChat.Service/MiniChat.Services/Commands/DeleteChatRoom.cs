7ce98e9b3d9d132c3035bc1f0de2256d1a0bcbf1
FremyXS <70861067+FremyXS@users.noreply.github.com>
2023-06-25 14:53:32 +0500

    feat(MiniChat.Services): add command DeleteChatRoom

---
+++ MiniChat.Service/MiniChat.Services/Commands/DeleteChatRoom.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiniChat.Database;
+using MiniChat.Service.Commands.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniChat.Service.Commands
+{
+    public class DeleteChatRoom: IDeleteChatRoom
+    {
+        private readonly ChatDbContext _chatDbContext;
+
+        public DeleteChatRoom(ChatDbContext chatDbContext)
+        {
+            _chatDbContext = chatDbContext;
+        }
+
+        public async Task<int> Invoke(long id)
+        {
+            var chat = await _chatDbContext.ChatRooms
+                .FirstOrDefaultAsync(el => el.Id == id);
+
+            if (chat == null)
+            {
+                throw new Exception($"Chat room not found with id: {id}");
+            }
+
+            chat = chat.SetDeleteDate();
+
+            _chatDbContext.Update(chat);
+
+            var res = await _chatDbContext.SaveChangesAsync();
+
+            return res;
+        }
+    }
+}

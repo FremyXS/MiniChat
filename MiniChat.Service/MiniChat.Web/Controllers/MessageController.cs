a086c178cecafd0df5ab53902ca074823b1b2df8
FremyXS <70861067+FremyXS@users.noreply.github.com>
2023-06-25 16:45:36 +0500

    feat(MiniChat.Web): add new controller MessageController

---
+++ MiniChat.Service/MiniChat.Web/Controllers/MessageController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MiniChat.Web.Controllers
+{
+    [Route("[controller]")]
+    public class MessageController : ControllerBase
+    {
+        [HttpGet("chat/{chatId:long}")]
+        public async Task<IActionResult> GetMessagesByChatId([FromRoute] long chatId)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateMessage()
+        {
+            return Ok();
+        }
+
+        [HttpPatch("{id:long}")]
+        public async Task<IActionResult> UpdateMessage([FromRoute] long id)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{id:long}")]
+        public async Task<IActionResult> DeleteMessage([FromRoute] long id)
+        {
+            return Ok();
+        }
+    }
+}

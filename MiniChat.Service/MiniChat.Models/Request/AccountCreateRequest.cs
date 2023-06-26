fd0fb07220c7fb2308b6ecd3a158b192fa9480f5
FremyXS <70861067+FremyXS@users.noreply.github.com>
2023-06-26 13:12:56 +0500

    feat(MiniChat.Models): add new request AccountCreateRequest

---
+++ MiniChat.Service/MiniChat.Models/Request/AccountCreateRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace MiniChat.Models.Request
+{
+    public record class AccountCreateRequest(
+        string Email,
+        string Login,
+        string Password,
+        string UserName,
+        string UserPhoto
+    );
+}

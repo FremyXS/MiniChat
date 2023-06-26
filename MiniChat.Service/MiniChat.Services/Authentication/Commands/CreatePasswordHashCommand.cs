a46ccfa07b8c19699558e5fcb01ee4a7fe5a53c1
FremyXS <70861067+FremyXS@users.noreply.github.com>
2023-06-26 13:13:58 +0500

    feat(MiniChat.Services): add new command CreatePasswordHashCommand

---
+++ MiniChat.Service/MiniChat.Services/Authentication/Commands/CreatePasswordHashCommand.cs
@@ -0,0 +1,26 @@
+﻿using MiniChat.Service.Authentication.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniChat.Service.Authentication.Commands
+{
+    public class CreatePasswordHashCommand: ICreatePasswordHashCommand
+    {
+        public void Invoke(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            if (password == null) 
+                throw new ArgumentNullException("password");
+            if (string.IsNullOrWhiteSpace(password)) 
+                throw new ArgumentException("Value cannot be empty or whitespace only string.", "password");
+
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+            }
+        }
+    }
+}
---
+++ MiniChat.Service/MiniChat.Services/Authentication/Common/ICreatePasswordHashCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniChat.Service.Authentication.Common
+{
+    public interface ICreatePasswordHashCommand
+    {
+        void Invoke(string password, out byte[] passwordHash, out byte[] passwordSalt);
+    }
+}

878aaee20913a1c874b22ad9d94215bebd836eb6
FremyXS <70861067+FremyXS@users.noreply.github.com>
2023-06-26 16:49:23 +0500

    feat(MiniChat.Services): add new service AuthenticationService

---
+++ MiniChat.Service/MiniChat.Services/Authentication/AuthenticationService.cs
@@ -0,0 +1,29 @@
+﻿using MiniChat.Models.Request;
+using MiniChat.Service.Authentication.Common;
+
+namespace MiniChat.Service.Authentication
+{
+    public class AuthenticationService: IAuthenticationService
+    {
+        private readonly ICreateAccountCommand _createAccountCommand;
+        private readonly IAuthenticateAccountCommand _authenticateAccount;
+
+        public AuthenticationService(
+            ICreateAccountCommand createAccountCommand,
+            IAuthenticateAccountCommand authenticateAccount)
+        {
+            _createAccountCommand = createAccountCommand;
+            _authenticateAccount = authenticateAccount;
+        }
+
+        public async Task<int> CreateAccount(AccountCreateRequest accountCreateRequest)
+        {
+            return await _createAccountCommand.Invoke(accountCreateRequest);
+        }
+
+        public async Task<string> AuthenticateAccount(AccountAuthenticateRequest accountAuthenticateRequest)
+        {
+            return await _authenticateAccount.Invoke(accountAuthenticateRequest);
+        }
+    }
+}
---
+++ MiniChat.Service/MiniChat.Services/Authentication/Common/IAuthenticationService.cs
@@ -0,0 +1,10 @@
+﻿using MiniChat.Models.Request;
+
+namespace MiniChat.Service.Authentication.Common
+{
+    public interface IAuthenticationService
+    {
+        Task<int> CreateAccount(AccountCreateRequest accountCreateRequest);
+        Task<string> AuthenticateAccount(AccountAuthenticateRequest accountAuthenticateRequest);
+    }
+}

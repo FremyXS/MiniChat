c922db2a0eb74b714421460276a31ca03d3b7553
FremyXS <70861067+FremyXS@users.noreply.github.com>
2023-06-26 13:13:22 +0500

    feat(MiniChat.Models): add new mapper AccountMapper

---
+++ MiniChat.Service/MiniChat.Models/Mappers/AccountMapper.cs
@@ -0,0 +1,32 @@
+﻿using Mapster;
+using MiniChat.Database.Entity;
+using MiniChat.Models.Dto;
+using MiniChat.Models.Request;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniChat.Models.Mappers
+{
+    public static class AccountMapper
+    {
+        static AccountMapper()
+        {
+            TypeAdapterConfig<AccountCreateRequest, User>
+                .NewConfig()
+                .Map(x => x.Name, src => src.UserName)
+                .Map(x => x.Photo, src => src.UserPhoto);
+        }
+        public static Account ToModel(this AccountCreateRequest accountCreateRequest)
+        {
+            return accountCreateRequest.Adapt<Account>();
+        }
+        public static User ToUserModel(this AccountCreateRequest accountCreateRequest)
+        {
+            return accountCreateRequest.Adapt<User>();
+        }
+
+    }
+}

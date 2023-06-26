484a4f2e61954c9eaf07e0298a1f2af6f7c9bcb5
FremyXS <70861067+FremyXS@users.noreply.github.com>
2023-06-26 15:14:31 +0500

    feat(MiniChat.Services): add new command CreateJwtTokenCommand

---
+++ MiniChat.Service/MiniChat.Services/Authentication/Commands/CreateJwtTokenCommand.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using MiniChat.Service.Authentication.Common;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Text;
+
+namespace MiniChat.Service.Authentication.Commands
+{
+    public class CreateJwtTokenCommand: ICreateJwtTokenCommand
+    {
+        public string Invoke(string accountLogin)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Issuer = JwtConfiguration.ISSUER,
+                Audience = JwtConfiguration.AUDIENCE,
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                new Claim(ClaimTypes.Name, accountLogin),
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(JwtConfiguration.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
+
+            return tokenString;
+        }
+    }
+}

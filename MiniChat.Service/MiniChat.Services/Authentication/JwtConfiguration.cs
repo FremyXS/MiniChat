455e5bad004eec0d11b2078708560d3f7b2e17bc
FremyXS <70861067+FremyXS@users.noreply.github.com>
2023-06-26 15:16:12 +0500

    feat(MiniChat.Services): add configuration JwtConfiguration

---
+++ MiniChat.Service/MiniChat.Services/Authentication/JwtConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniChat.Service.Authentication
+{
+    public class JwtConfiguration
+    {
+        public const string ISSUER = "MyAuthServer"; // издатель токена
+        public const string AUDIENCE = "MyAuthClient"; // потребитель токена
+        const string KEY = "mysupersecret_secretkey!123";   // ключ для шифрации
+        public static SymmetricSecurityKey GetSymmetricSecurityKey() =>
+            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(KEY));
+    }
+}

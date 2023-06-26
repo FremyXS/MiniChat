ff2002c6b79db317b7edb0b061aa4acc767ea21a
FremyXS <70861067+FremyXS@users.noreply.github.com>
2023-06-26 17:03:09 +0500

    refactor(MiniChat.Services): add request validators to AuthenticationService

--- MiniChat.Service/MiniChat.Services/Authentication/AuthenticationService.cs
+++ MiniChat.Service/MiniChat.Services/Authentication/AuthenticationService.cs
@@ -1,4 +1,6 @@
-﻿using MiniChat.Models.Request;
+﻿using FluentValidation;
+using MiniChat.Models.Request;
+using MiniChat.Models.Validators;
 using MiniChat.Service.Authentication.Common;
 
 namespace MiniChat.Service.Authentication
@@ -7,22 +9,42 @@ namespace MiniChat.Service.Authentication
     {
         private readonly ICreateAccountCommand _createAccountCommand;
         private readonly IAuthenticateAccountCommand _authenticateAccount;
+        private readonly AccountAuthenticateRequestValidator _accountAuthenticateRequestValidator;
+        private readonly AccountCreateRequestValidator _accountCreateRequestValidator;
 
         public AuthenticationService(
             ICreateAccountCommand createAccountCommand,
-            IAuthenticateAccountCommand authenticateAccount)
+            IAuthenticateAccountCommand authenticateAccount,
+            AccountCreateRequestValidator accountCreateRequestValidator,
+            AccountAuthenticateRequestValidator accountAuthenticateRequestValidator)
         {
             _createAccountCommand = createAccountCommand;
             _authenticateAccount = authenticateAccount;
+            _accountCreateRequestValidator = accountCreateRequestValidator;
+            _accountAuthenticateRequestValidator = accountAuthenticateRequestValidator;
         }
 
         public async Task<int> CreateAccount(AccountCreateRequest accountCreateRequest)
         {
+            var validResult = await _accountCreateRequestValidator.ValidateAsync(accountCreateRequest);
+
+            if (!validResult.IsValid)
+            {
+                throw new ValidationException(validResult.Errors);
+            }
+
             return await _createAccountCommand.Invoke(accountCreateRequest);
         }
 
         public async Task<string> AuthenticateAccount(AccountAuthenticateRequest accountAuthenticateRequest)
         {
+            var validResult = await _accountAuthenticateRequestValidator.ValidateAsync(accountAuthenticateRequest);
+
+            if (!validResult.IsValid)
+            {
+                throw new ValidationException(validResult.Errors);
+            }
+
             return await _authenticateAccount.Invoke(accountAuthenticateRequest);
         }
     }

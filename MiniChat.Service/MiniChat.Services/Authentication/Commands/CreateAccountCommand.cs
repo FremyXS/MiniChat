025eabe63fbdfda813c30fd84dfdf8850a7abfe3
FremyXS <70861067+FremyXS@users.noreply.github.com>
2023-06-26 13:14:32 +0500

    feat(MiniChat.Services): add new command CreateAccountCommand

---
+++ MiniChat.Service/MiniChat.Services/Authentication/Commands/CreateAccountCommand.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiniChat.Database;
+using MiniChat.Models.Mappers;
+using MiniChat.Models.Request;
+using MiniChat.Service.Authentication.Common;
+
+namespace MiniChat.Service.Authentication.Commands
+{
+    public class CreateAccountCommand: ICreateAccountCommand
+    {
+        private readonly ChatDbContext _chatDbContext;
+        private readonly ICreatePasswordHashCommand _createPasswordHashCommand;
+
+        public CreateAccountCommand(ChatDbContext chatDbContext, ICreatePasswordHashCommand createPasswordHashCommand)
+        {
+            _chatDbContext = chatDbContext;
+            _createPasswordHashCommand = createPasswordHashCommand;
+        }
+
+        public async Task<int> Invoke(AccountCreateRequest accountCreateRequest)
+        {
+            var findAccount = await _chatDbContext.Accounts
+                .FirstOrDefaultAsync(el => el.Email.Contains(accountCreateRequest.Email) || el.Login.Contains(accountCreateRequest.Login));
+
+            if (findAccount != null)
+            {
+                throw new ArgumentException($"This login: {accountCreateRequest.Login} or email: {accountCreateRequest.Email} is already used");
+            }
+
+            byte[] passwordHash, passwordSalt;
+            _createPasswordHashCommand.Invoke(accountCreateRequest.Password, out passwordHash, out passwordSalt);
+
+            var user = accountCreateRequest.ToUserModel();
+            user.SetCreateTime();
+            var account = accountCreateRequest.ToModel();
+
+            account.PasswordHash = passwordHash;
+            account.PasswordSalt = passwordSalt;
+
+            var userEntity = await _chatDbContext.Users.AddAsync(user);
+            account.UserId = userEntity.Entity.Id;
+            account.User = userEntity.Entity;
+            account.SetCreateTime();
+
+            var accountEntity = await _chatDbContext.Accounts.AddAsync(account);
+
+            var res = await _chatDbContext.SaveChangesAsync();
+
+            return res;
+        }
+    }
+}
---
+++ MiniChat.Service/MiniChat.Services/Authentication/Common/ICreateAccountCommand.cs
@@ -0,0 +1,14 @@
+﻿using MiniChat.Models.Request;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniChat.Service.Authentication.Common
+{
+    public interface ICreateAccountCommand
+    {
+        Task<int> Invoke(AccountCreateRequest accountCreateRequest);
+    }
+}
